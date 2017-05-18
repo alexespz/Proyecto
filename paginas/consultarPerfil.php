1b2b7ac302e590a40780e9ee9e429fb1dc45ab0a
alexespz <alexespz@gmail.com>
2017-05-18 10:43:59 +0200

    Update consultarPerfil.php

--- paginas/consultarPerfil.php
+++ paginas/consultarPerfil.php
@@ -4,6 +4,31 @@ include_once '../procedimientos/procedimientos.php';
 include_once '../consultas/obtenerPerfil.php';
 
 ?>
+<script>
+    function modificarUsuario(user) {
+        $.ajax({
+            async: true,
+            type: "POST",
+            url: "../paginas/modificarUsuario.php",
+            data: "usuario="+user,
+            success: function(resp){
+                $('#cuerpo').load("modificarUsuario.php");
+            }
+        });
+    }
+    
+    function modificarPerfil(nombre, apellidos, email, telefono, sexo, dni, submit) {
+        $.ajax({
+            async: true,
+            type: "POST",
+            url: "../paginas/modificarPerfil.php",
+            data: "nombre="+nombre+"&apellidos="+apellidos+"&email="+email+"&telefono="+telefono+"&sexo="+sexo+"&dni="+dni+"&submit="+submit,
+            success: function(resp){
+                $('#cuerpo').load("modificarPerfil.php");
+            }
+        });
+    }
+</script>
 <html lang="en">
     <div class="col-md-9 pull-md-right main-content">
         <div class="col-md-12 text-center"><h1><p>PERFIL PERSONAL</p></h1></div>
@@ -12,51 +37,51 @@ include_once '../consultas/obtenerPerfil.php';
                 <div class="col-md-6 form-group">
                     <div class="form-group">
                         <label for="nombre" class="control-label">Nombre</label>
-                        <input type="text" name="nombre" id="nombre" class="form-control" maxlength="20" value="<?php echo $nombre ?>" disabled>
+                        <input type="text" name="nombre" id="nombre" class="form-control" value="<?php echo $nombre ?>" disabled>
                     </div>
                     <div class="form-group">
                         <label for="email" class="control-label">Dirección email</label>
-                        <input type="text" name="email" id="email" class="form-control" placeholder="Correo electrónico *" maxlength="60" required>
+                        <input type="text" name="email" id="email" class="form-control" value="<?php echo $email ?>" disabled>
                     </div>
                     <div class="form-group">
                         <label for="sexo" class="control-label">Sexo</label>
-                        <select name="sexo" class="form-control" id="sexo" required>
-                            <option value="Hombre">Hombre</option>
-                            <option value="Mejer">Mujer</option>
-                        </select>
+                        <?php if($sexo == 'H'){
+                            $sexo = "Hombre";
+                        }else{
+                            $sexo = "Mujer";
+                        }?>
+                        <input type="text" name="sexo" id="sexo" class="form-control" value="<?php echo $sexo ?>" disabled>
                     </div>
                 </div>
                 <div class="col-md-6 form-group">
                     <div class="form-group">
                         <label for="apellidos" class="control-label">Apellidos</label>
-                        <input type="text" name="apellidos" id="apellidos" class="form-control" placeholder="Apellidos *" maxlength="40" required>
+                        <input type="text" name="apellidos" id="apellidos" class="form-control" value="<?php echo $apellidos ?>" disabled>
                     </div>
                     <div class="form-group">
                         <label for="telefono" class="control-label">Telefono</label>
-                        <input type="text" name="telefono" id="telefono" class="form-control" placeholder="Telefono *" maxlength="9" required>
+                        <input type="text" name="telefono" id="telefono" class="form-control" value="<?php echo $telefono ?>" disabled>
                     </div>
                     <div class="form-group">
                         <label for="dni" class="control-label">DNI</label>
-                        <input type="text" name="dni" id="dni" class="form-control" placeholder="DNI *" maxlength="9" required>
+                        <input type="text" name="dni" id="dni" class="form-control" value="<?php echo $dni ?>" disabled>
                     </div>
                 </div>
                 <div class="divider"></div>
                 <div class="col-md-8 form-group">
                     <div class="form-group">
                         <label for="usuario" class="control-label">Usuario</label>
-                        <input type="text" name="usuario" id="usuario" class="form-control" placeholder="Usuario *" maxlength="25" required>
+                        <input type="text" name="usuario" id="usuario" class="form-control" value="<?php echo $usuario ?>" disabled>
                     </div>
                 </div>
                 <div class="col-md-4 form-group">
                     <div class="form-group">
-                        <button class="btn btn-info" id="submit" style="margin-top: 25px;" onclick="">Modificar usuario</button>
+                        <button class="btn btn-info" id="submit" style="margin-top: 25px;" onclick="modificarUsuario(document.getElementById('usuario').value);">Cambiar usuario</button>
                     </div>
                 </div>
             </div>
-
-            </div>
             <div class="col-md-12 " id="boton">
-                <button class="btn btn-info" id="submit" onclick="Validar(document.getElementById('nombre').value, document.getElementById('apellidos').value, document.getElementById('email').value, document.getElementById('telefono').value, document.getElementById('sexo').value, document.getElementById('dni').value, document.getElementById('usuario').value, document.getElementById('contrasenia').value, document.getElementById('contrasenia2').value, document.getElementById('submit').value);">Entrar</button>
+                <button class="btn btn-info" id="submit" onclick="modificarPerfil(document.getElementById('nombre').value, document.getElementById('apellidos').value, document.getElementById('email').value, document.getElementById('telefono').value, document.getElementById('sexo').value, document.getElementById('dni').value, document.getElementById('submit').value);">Modificar datos</button>
             </div>
             <div class="col-md-12 espacios" id="resultado"></div>
         </form>
