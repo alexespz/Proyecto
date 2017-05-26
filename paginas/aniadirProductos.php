51300923b825bedcddfb2f4cbf3854de4e19647a
alexespz <alexespz@gmail.com>
2017-05-26 12:55:43 +0200

    Create aniadirProductos.php

---
+++ paginas/aniadirProductos.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+include_once '../procedimientos/procedimientos.php';
+
+$conexion = new procedimientos();
+$conexion->conect();
+
+if(!isset($_POST["usuario"])){
+  header("Location: ../paginas/paginaAdministrador.php");
+}
+
+?>
+<script>
+    function aniadirProducto(nombre, descripcion, precio, calorias, foto, tipo) {
+        $.ajax({
+            async: true,
+            type: "POST",
+            url: "../consultas/aniadirProducto.php",
+            data: "nombre="+nombre+"&descripcion="+descripcion+"&precio="+precio+"&calorias="+calorias+"&foto="+foto+"&tipo="+tipo,
+            success: function(resp){
+                $('#resultado').html(resp);
+            }
+        });
+    }
+    
+    function volver(){
+        window.location = "../paginas/paginaPrincipal.php";
+    }
+</script>
+<html lang="en">
+<div class="col-md-9 pull-md-right main-content">
+    <div class="col-md-12 text-center"><h1><p>AÑADIR PRODUCTO</p></h1></div>
+    <form action="return false" onsubmit="return false" method="POST" enctype="multipart/form-data">
+        <input type="hidden" name="MAX_FILE_SIZE" value="16000000"/>
+        <div class="row">
+            <div class="col-md-6 form-group">
+                <div class="form-group">
+                    <label for="nombre" class="control-label">Nombre</label>
+                    <input type="text" name="nombre" id="nombre" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="precio" class="control-label">Precio</label>
+                    <input type="text" name="precio" id="precio" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="tipo" class="control-label">Tipo de producto</label>
+                    <select name="tipo" class="form-control" id="tipo" required>
+                      <?php
+                      $query = "SELECT * FROM tipo_producto";
+                      $conexion->consultas($query);
+                      while($resultado = $conexion->devolverFilas()){
+                        echo '<option value="'.$resultado["id_tipo_producto"].'">'.$resultado["nombre"].'</option>';
+                      }?>
+                </div>
+            </div>
+            <div class="col-md-6 form-group">
+                <div class="form-group">
+                    <label for="descripcion" class="control-label">Descripcion</label>
+                    <input type="text" name="descripcion" id="descipcion" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="calorias" class="control-label">Calorias</label>
+                    <input type="text" name="calorias" id="calorias" class="form-control">
+                </div>
+            </div>
+            <div class="divider"></div>
+            <div class="col-md-8 form-group">
+                <div class="form-group">
+                    <label for="foto" class="control-label">Imagen de la preferencia</label>
+		                <input type="file" name="imagen" id="foto" class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 " id="boton">
+            <button class="btn btn-info" id="submit" onclick="aniadirProducto($('#nombre').text(), $('#descripcion').text(), $('#precio').text(), $('#calorias').text(), $('#foto').text(), $('#tipo').val();">Añadir</button>
+            <button class="btn btn-info" id="submit" onclick="volver();">Volver al Menu</button>
+        </div>
+        <div class="col-md-12 espacios" id="resultado"></div>
+    </form>
+</div>
+</html>
