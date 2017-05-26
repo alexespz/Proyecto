68d988b17872f0b3ac987f728e3cae86efcf2b77
alexespz <alexespz@gmail.com>
2017-05-26 09:57:39 +0200

    Update ajaxAdmin.js

--- sources/ajaxAdmin.js
+++ sources/ajaxAdmin.js
@@ -1,6 +1,6 @@
 function modificarPerfil(){
   $('#listadoProductos').on("click", function(e){
         e.preventDefault();
-        $('#cuerpo').load("../consultas/listadoProductos.php");
+        $('#cuerpo').load("listadoProductos.php");
   });
 }
