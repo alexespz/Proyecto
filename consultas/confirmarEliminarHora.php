b95239ca2f2c6ed73721f3b8a0c2419fdbfa2c21
Alejandro Flores <alexeszp@gmail.com>
2017-06-05 12:33:18 +0200

    ADD OPTION TO RECOVER WHEN IS DELETE

---
+++ consultas/confirmarEliminarHora.php
@@ -0,0 +1,31 @@
+<?php
+include_once '../procedimientos/procedimientos.php';
+$conexion = new procedimientos();
+$conexion->conect();
+?>
+<script>
+    function eliminarHora(id) {
+        $.ajax({
+            async: true,
+            type: "POST",
+            url: "../consultas/eliminarHora.php",
+            data: "id=" + id,
+            success: function (resp) {
+
+            }
+        });
+    }
+</script>
+<?php
+echo '
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <h4 class="modal-title" id="Modal_Label_' . $_GET["id"] . '">Aviso</h4>
+</div>
+<div class="modal-body col-md-12">
+    <p>¿Seguro que desea eliminar el elemento seleccionado?</p>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-primary pull-right" onclick="eliminarHora('.$_GET["id"].')">Si</button>
+    <button type="button" class="btn btn-danger pull-left" data-dismiss="modal">No</button>
+</div>';
